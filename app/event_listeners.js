ebeac6edcad32a1b09107ac10ca6e2b2d9850617
Nishanth Vijayan <nishanththegr8@gmail.com>
2017-02-21 20:06:02 +0530

    Add a small 3 sec delay in opening the notifications

--- app/event_listeners.js
+++ app/event_listeners.js
@@ -2,6 +2,8 @@ var Feedback = require('./feedback');
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.request == "askForFeedback") {
-        Feedback.init();
+        setTimeout(function(){
+            Feedback.init();
+        }, 3000);
     }
 });
