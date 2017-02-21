5ad5633745952ef80191bbe9ffd241f5eab59ca7
Nishanth Vijayan <nishanththegr8@gmail.com>
2017-02-22 00:41:01 +0530

    Display No Hidden Contests when no contests are hidden

--- app/components/Containers/ArchiveContainer.js
+++ app/components/Containers/ArchiveContainer.js
@@ -11,18 +11,28 @@ var Archive = React.createClass({
             upcoming: Cache.fetch().data.upcoming.filter(function(contest){return Hide.isHidden(contest);})
         }
 
-        return(
-            <div className = 'archive-container'>
-                <div id='ongoing' className="top-title">
-                    <ContestTypeHeader type="Hidden Live" />
-                    <ContestList contests={hiddenContests.ongoing} type='live' />
+        if((hiddenContests.ongoing.length + hiddenContests.upcoming.length) > 0){
+            return(
+                <div className = 'archive-container'>
+                    <div id='ongoing' className="top-title">
+                        <ContestTypeHeader type="Hidden Live" />
+                        <ContestList contests={hiddenContests.ongoing} type='live' />
+                    </div>
+                    <div id='upcoming'>
+                        <ContestTypeHeader type="Hidden Upcoming" />
+                        <ContestList contests={hiddenContests.upcoming} type='upcoming' />
+                    </div>
                 </div>
-                <div id='upcoming'>
-                    <ContestTypeHeader type="Hidden Upcoming" />
-                    <ContestList contests={hiddenContests.upcoming} type='upcoming' />
+            )
+        }else{
+            return(
+                <div className = 'archive-container'>
+                    <div id='ongoing' className="top-title">
+                        <ContestTypeHeader type="No Hidden" />
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        }
     }
 });
 
