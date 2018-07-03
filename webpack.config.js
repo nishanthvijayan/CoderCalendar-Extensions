4f2adf9c4251e0e0de3e8e5b34aab9b4ce939a94
Nishanth Vijayan <nishanthvijayan1995@gmail.com>
2018-07-03 14:26:55 +0530

    Unify build process for firefox & chrome

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,58 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-module.exports = {
-	entry: {
-		"index" : "./app/index.js",
-		"notifier_service": "./app/notifier_service.js",
-		"event_listeners": "./app/event_listeners.js"
+module.exports = [
+	{
+    name: "chrome",
+    entry: {
+      "index" : "./app/index.js",
+      "notifier_service": "./app/notifier_service.js",
+      "event_listeners": "./app/event_listeners.js"
 
-	},
-	output:{
-		path: __dirname + '/dist/', 
-		filename: 'js/[name].js'
-	},
-	module: {
-		rules: [
-			{ test: /\.js$/, exclude: /node_modules/ , use: 'babel-loader' }
-		]
-	},
-	plugins: [
-		new CopyWebpackPlugin([
-		  { from: 'dist/css/index.css', to: 'css/mozilla.css' },
-		])
-	]
-	  
-}
+    },
+    output:{
+      path: __dirname + '/build/chrome', 
+      filename: 'js/[name].js'
+    },
+    module: {
+      rules: [
+        { test: /\.js$/, exclude: /node_modules/ , use: 'babel-loader' }
+      ]
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        { from: 'css/chrome.css', to: 'css/index.css' },
+        { from: 'index.html', to: 'index.html' },
+        { from: 'manifest.json', to: 'manifest.json' },
+        { from: 'img', to: 'img' },
+        { from: 'font-awesome', to: 'font-awesome' }
+      ])
+    ]
+  },
+  {
+    name: "firefox",
+    entry: {
+      "index" : "./app/index.js",
+      "notifier_service": "./app/notifier_service.js",
+      "event_listeners": "./app/event_listeners.js"
+
+    },
+    output:{
+      path: __dirname + '/build/firefox', 
+      filename: 'js/[name].js'
+    },
+    module: {
+      rules: [
+        { test: /\.js$/, exclude: /node_modules/ , use: 'babel-loader' }
+      ]
+    },
+    plugins: [
+      new CopyWebpackPlugin([
+        { from: 'css/firefox.css', to: 'css/index.css' },
+        { from: 'index.html', to: 'index.html' },
+        { from: 'manifest.json', to: 'manifest.json' },
+        { from: 'img', to: 'img' },
+        { from: 'font-awesome', to: 'font-awesome' }
+      ])
+    ]
+	}
+]
