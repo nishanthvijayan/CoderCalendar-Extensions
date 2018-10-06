5cd449ee36281d15937145ade30896c5534b1f71
Nishanth Vijayan <nishanthvijayan1995@gmail.com>
2018-10-06 17:18:05 +0530

    Make Header use destructures props

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,24 +1,21 @@
 const React = require('react');
 
-const Header = React.createClass({
-  refreshButtonSpinState() {
-    if (this.props.isLoading) {
-      return 'fa fa-refresh fa-2x fa-spin';
-    }
+const Header = ({
+  isLoading, onClickArchive, onClickListings, onClickDonate, onClickRefresh, onClickSettings,
+}) => {
+  const refreshButtonSpinState = () => (
+    isLoading ? 'fa fa-refresh fa-2x fa-spin' : 'fa fa-refresh fa-2x'
+  );
 
-    return 'fa fa-refresh fa-2x';
-  },
-  render() {
-    return (
-      <header>
-        <i className="fa fa-home fa-2x" onClick={this.props.onClickListings} title="Home" />
-        <i className="fa fa-trash archive-icon fa-2x" onClick={this.props.onClickArchive} title="Hidden Contests" />
-        <i className="fa fa-beer fa-2x" onClick={this.props.onClickDonate} title="Buy me a Beer!" />
-        <i className="fa fa-gear fa-2x" onClick={this.props.onClickSettings} title="Settings" />
-        <i className={this.refreshButtonSpinState()} onClick={this.props.onClickRefresh} title="Refresh" />
-      </header>
-    );
-  },
-});
+  return (
+    <header>
+      <i className="fa fa-home fa-2x" onClick={onClickListings} title="Home" />
+      <i className="fa fa-trash archive-icon fa-2x" onClick={onClickArchive} title="Hidden Contests" />
+      <i className="fa fa-beer fa-2x" onClick={onClickDonate} title="Buy me a Beer!" />
+      <i className="fa fa-gear fa-2x" onClick={onClickSettings} title="Settings" />
+      <i className={refreshButtonSpinState()} onClick={onClickRefresh} title="Refresh" />
+    </header>
+  );
+};
 
 module.exports = Header;
