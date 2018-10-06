36d306d20187a023153141246b91a7a978922971
Nishanth Vijayan <nishanthvijayan1995@gmail.com>
2018-10-06 17:23:00 +0530

    Simplify AddToCalendarButton component

--- app/components/Contest/AddToCalendarButton.js
+++ app/components/Contest/AddToCalendarButton.js
@@ -1,25 +1,23 @@
 const React = require('react');
 
-const AddToCalendarButton = React.createClass({
-  onClickAddToCalendarButton() {
+const AddToCalendarButton = (contest) => {
+  const onClickAddToCalendarButton = () => {
     ga('send', 'event', 'Add to Calendar');
-    chrome.tabs.create({ url: this.state.url });
-  },
-  componentDidMount() {
-    if (this.props.type == 'upcoming') {
-      this.setState({
-        url: this.props.contest.createGoogleAddToCalendarUrl(),
-      });
-    }
-  },
-  render() {
-    if (this.props.type == 'upcoming' && this.props.visible) {
-      return (
-        <i className="fa fa-calendar fa-lg option-icon circular-border" style={{ color: '#4caf50', cursor: 'pointer' }} onClick={this.onClickAddToCalendarButton} title="Add to Google Calendar" />
-      );
-    }
-    return null;
-  },
-});
+    chrome.tabs.create({ url: contest.createGoogleAddToCalendarUrl() });
+  };
+
+  if (this.props.type == 'upcoming' && this.props.visible) {
+    return (
+      <i
+        className="fa fa-calendar fa-lg option-icon circular-border"
+        style={{ color: '#4caf50', cursor: 'pointer' }}
+        onClick={onClickAddToCalendarButton}
+        title="Add to Google Calendar"
+      />
+    );
+  }
+
+  return null;
+};
 
 module.exports = AddToCalendarButton;
