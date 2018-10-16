bcfbe5cc36e46839b9e58c8a0a1fe1358aaa5664
Nishanth Vijayan <nishanthvijayan1995@gmail.com>
2018-10-16 23:53:10 +0530

    Make ArchiveContainer pure functional component

--- app/components/Containers/ArchiveContainer.js
+++ app/components/Containers/ArchiveContainer.js
@@ -4,40 +4,38 @@ const ContestList = require('./../ContestList');
 const store = require('../../store');
 const Contest = require('../../Contest');
 
-const Archive = React.createClass({
-  render() {
-    const { ongoing: ongoingContests, upcoming: upcomingContests } = store.getContests().data;
-    const hiddenContests = {
-      ongoing: ongoingContests
-        .map(contestJson => new Contest(contestJson))
-        .filter(contest => contest.isHidden()),
-      upcoming: upcomingContests
-        .map(contestJson => new Contest(contestJson))
-        .filter(contest => contest.isHidden()),
-    };
+const Archive = () => {
+  const { ongoing: ongoingContests, upcoming: upcomingContests } = store.getContests().data;
+  const hiddenContests = {
+    ongoing: ongoingContests
+      .map(contestJson => new Contest(contestJson))
+      .filter(contest => contest.isHidden()),
+    upcoming: upcomingContests
+      .map(contestJson => new Contest(contestJson))
+      .filter(contest => contest.isHidden()),
+  };
 
-    if ((hiddenContests.ongoing.length + hiddenContests.upcoming.length) > 0) {
-      return (
-        <div className="archive-container">
-          <div id="ongoing" className="top-title">
-            <ContestTypeHeader type="Hidden Live" />
-            <ContestList contests={hiddenContests.ongoing} type="live" route="archive" />
-          </div>
-          <div id="upcoming">
-            <ContestTypeHeader type="Hidden Upcoming" />
-            <ContestList contests={hiddenContests.upcoming} type="upcoming" route="archive" />
-          </div>
-        </div>
-      );
-    }
+  if ((hiddenContests.ongoing.length + hiddenContests.upcoming.length) > 0) {
     return (
       <div className="archive-container">
         <div id="ongoing" className="top-title">
-          <ContestTypeHeader type="No Hidden" />
+          <ContestTypeHeader type="Hidden Live" />
+          <ContestList contests={hiddenContests.ongoing} type="live" route="archive" />
+        </div>
+        <div id="upcoming">
+          <ContestTypeHeader type="Hidden Upcoming" />
+          <ContestList contests={hiddenContests.upcoming} type="upcoming" route="archive" />
         </div>
       </div>
     );
-  },
-});
+  }
+  return (
+    <div className="archive-container">
+      <div id="ongoing" className="top-title">
+        <ContestTypeHeader type="No Hidden" />
+      </div>
+    </div>
+  );
+};
 
 module.exports = Archive;
