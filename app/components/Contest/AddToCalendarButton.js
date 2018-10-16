0ce5870b7c92c48b543e5f7bfaf43acd5180fed3
Nishanth Vijayan <nishanthvijayan1995@gmail.com>
2018-10-16 23:43:16 +0530

    Fix bug where this.prop was being accessed in pure functional component

--- app/components/Contest/AddToCalendarButton.js
+++ app/components/Contest/AddToCalendarButton.js
@@ -1,23 +1,22 @@
 const React = require('react');
 
-const AddToCalendarButton = (contest) => {
+const AddToCalendarButton = (contest, type, visible) => {
   const onClickAddToCalendarButton = () => {
     ga('send', 'event', 'Add to Calendar');
     chrome.tabs.create({ url: contest.createGoogleAddToCalendarUrl() });
   };
 
-  if (this.props.type == 'upcoming' && this.props.visible) {
-    return (
-      <i
-        className="fa fa-calendar fa-lg option-icon circular-border"
-        style={{ color: '#4caf50', cursor: 'pointer' }}
-        onClick={onClickAddToCalendarButton}
-        title="Add to Google Calendar"
-      />
-    );
+  if (!visible || type != 'upcoming') {
+    return null;
   }
-
-  return null;
+  return (
+    <i
+      className="fa fa-calendar fa-lg option-icon circular-border"
+      style={{ color: '#4caf50', cursor: 'pointer' }}
+      onClick={onClickAddToCalendarButton}
+      title="Add to Google Calendar"
+    />
+  );
 };
 
 module.exports = AddToCalendarButton;
